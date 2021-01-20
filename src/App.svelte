40f634febbd99f5208ec5d288305ba2b4cd486bf
Josh Nussbaum <joshnuss@gmail.com>
2021-01-19 21:52:02 -0500

    Styles login page

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,31 @@
 <script>
+  import { Route } from 'tinro'
+  import db from './db'
   import Board from './Board.svelte'
+  import BoardList from './BoardList.svelte'
+  import Login from './Login.svelte'
+
+  let user = db.user
 </script>
 
-<Board/>
+<svelte:head>
+  <title>Kanban</title>
+</svelte:head>
+
+{#if $user}
+  <button on:click={() => db.signOut()}>Sign out</button>
+  <Route path="/">
+    <BoardList/>
+  </Route>
+  <Route path="/:id" let:meta>
+    <Board id={meta.params.id}/>
+  </Route>
+{:else}
+  <Route path="/">
+    <Login/>
+  </Route>
+{/if}
+
+<Route fallback>
+  Aw shucks. That couldn't be found.
+</Route>
