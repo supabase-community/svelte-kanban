7071f9e04e30d78adfbef42d1f6b817885957cef
Josh Nussbaum <joshnuss@gmail.com>
2021-01-19 20:11:35 -0500

    Adds Login and BoardList components

---
+++ src/BoardList.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <title>Boards</title>
+</svelte:head>
+
+<h1>Boards</h1>
---
+++ src/Login.svelte
@@ -0,0 +1,24 @@
+<script>
+  import db from './db'
+
+  let email, sent = false
+
+  async function submit() {
+    await db.signIn(email)
+    sent = true
+  }
+</script>
+
+{#if sent}
+  Notification sent
+{:else}
+  <h1>Sign in</h1>
+
+  <form on:submit|preventDefault={submit}>
+    <label>
+      E-mail Address
+      <input type="email" required bind:value={email}/>
+    </label>
+    <button>Login</button>
+  </form>
+{/if}
