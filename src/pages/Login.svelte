82c4bc394a5d839df109626de273c6519a1f1931
Josh Nussbaum <joshnuss@gmail.com>
2021-01-20 22:25:54 -0500

    Update copy

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -25,7 +25,7 @@
         <svg class="icon" height=32 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-        <h2>Sign-in link sent, check your email</h2>
+        <h2>Sent you a link, check your email</h2>
       </div>
     </div>
   {:else}
